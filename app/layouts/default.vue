8a94e0a3dfd928603cbce3487288ce26f5fda6d8
Lasse Lambertsen <lasse@lambertsen.biz>
2022-02-28 20:10:43 +0100

    .

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <main :class="[$route.name]" class="main px-4 md:px-0">
+    <site-header />
+
     <div class="container mx-auto">
-      <site-header />
       <nuxt class="nuxt" />
     </div>
 
